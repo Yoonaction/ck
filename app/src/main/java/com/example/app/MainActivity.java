f369b28e660af8a6b379b900eb75d73aec5bb5e3
Yoonaction <790108973@qq.com>
2022-10-17 14:42:39 +0800

    commit message

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv=findViewById(R.id.textview);
+        tv.setText(String.valueOf(sum(100)));
+
+
+    }
+    public int sum(int n) {
+        int s = 0;
+        int i = 1;
+        while (i <= n) {
+            s += i;
+            i++;
+            }
+        System.out.println("1到n累加的和为：" + s);
+         return  s;
+        }
+
+}
